48c46293fbaa2f02f2945fbeb4ad214f68fe241f
Melia Masek <Masekmelia@gmail.com>
2022-10-16 16:25:24 -0600

    button changes

--- FrogMatch/Assets/Scripts/ChangeScene.cs
+++ FrogMatch/Assets/Scripts/ChangeScene.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+//code borrowed from Hooson "Change Scene On Button Click In 2 Minutes - Easy Unity Tutorial" off of youtube.
 public class ChangeScene : MonoBehaviour
 {
     public void MoveToScene(int sceneID)
