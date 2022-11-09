00d1cad172ea14c847e225cd63129861598f3439
Melia Masek <Masekmelia@gmail.com>
2022-11-08 23:08:26 -0700

    updated index to correct size, changed ai sprite and code

--- FrogMatch/Assets/Scripts/AgentBehaviour.cs
+++ FrogMatch/Assets/Scripts/AgentBehaviour.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
+//Code borrowed from and modified user robertbu on UnityAnswers "https://answers.unity.com/questions/630670/rotate-2d-sprite-towards-moving-direction.html"
+
 public class AgentBehaviour : MonoBehaviour
 {
     private NavMeshAgent agent;
@@ -16,6 +18,11 @@ public class AgentBehaviour : MonoBehaviour
 
     private void Update()
     {
-        //agent.destination = player.transform;
+        Vector3 moveDirection = gameObject.transform.position; 
+        if (moveDirection != Vector3.zero) 
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }    
     }
 }
